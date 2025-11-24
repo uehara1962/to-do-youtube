598ae6e8ae70a85663f2a6b00ff11b20239a8fe9
uehara1962 <ca_uehara@hotmail.com>
2025-11-24 18:22:54 -0300

    descomentada a autenticação antes da renderização

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,16 @@ import { Suspense } from "react";
 import { Spinner } from "@/components/ui/spinner";
 import ToDoListTable from "@/components/ToDoListTable";
 import LogoutButton from "@/components/LogoutButton";
-// import { getCurrentUser } from "@/lib/auth-server";
-// import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth-server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-  // console.log("home page user:", user);
-  // if (!user) {
-  //   redirect("/login");
-  // }
+  console.log("home page user:", user);
+  if (!user) {
+    redirect("/login");
+  }
 
   return (
     <Suspense fallback={<Spinner className={clsx("min-h-[200px]")} />}>
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -3,16 +3,16 @@ import ToDoList from "@/components/ToDoList";
 import { clsx } from "clsx";
 import { Suspense } from "react";
 import { Spinner } from "@/components/ui/spinner";
-// import { getCurrentUser } from "@/lib/auth-server";
-// import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth-server";
+import { redirect } from "next/navigation";
 
 export default async function TodoPage() {
   // Verificar autenticação antes de renderizar
-  // const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-  // if (!user) {
-  //   redirect("/login");
-  // }
+  if (!user) {
+    redirect("/login");
+  }
 
   return (
     <Suspense fallback={<Spinner className={clsx("min-h-[200px]")} />}>
