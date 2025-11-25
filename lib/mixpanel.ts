727fdbe16b2145b3255c2c01be4ceb280d17011f
uehara1962 <ca_uehara@hotmail.com>
2025-11-25 17:24:20 -0300

    correcao do  mixpanel

--- lib/mixpanel.ts
+++ lib/mixpanel.ts
@@ -2,15 +2,14 @@ import Mixpanel from "mixpanel";
 
 const mixpanelEvent = Mixpanel.init(process.env.MIXPANEL_TOKEN!);
 
-export function trackServerEvent(eventName: string, properties: Record<string, unknown>) {
-  
+export function trackServerEvent(
+  eventName: string,
+  properties: Record<string, unknown>
+) {
   if (process.env.NODE_ENV === "production") {
-  mixpanelEvent.track(eventName, properties);
+    mixpanelEvent.track(eventName, properties);
+  }
 }
-
-
-
-
 
 // ------------ Mixpanel ------------
 // import mixpanel from "mixpanel-browser";
